380f70089350f3fe175398e73c6a62692fd308a5
AniketShelar22 <shelar.aniket.comp@kgce.edu.in>
2025-07-18 15:45:09 +0530

    changeservejs

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const allowedOrigins = ['https://mentalhealthfrontend.netlify.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
+
 
 dotenv.config();
 connectDB();
